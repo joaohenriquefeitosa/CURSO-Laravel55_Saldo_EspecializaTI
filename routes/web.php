071b31b18f29a1dffd25406e5784267f5cf2e9bd
John Feitosa <joaohenriquefsf@gmail.com>
2019-04-23 00:08:33 -0300

    Aula 16 Concluída

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,9 @@ Route::get('/', 'Site\SiteController@index')->name('home');
 Auth::routes();
 
 
-Route::group(['middleware' => 'auth', 'namespace' => 'Admin'], function() {
-    Route::get('/admin', 'AdminController@index')->name('admin.home');
+Route::group(['middleware' => 'auth', 'namespace' => 'Admin', 'prefix' => 'admin'], function() {
+    Route::get('/', 'AdminController@index')->name('admin.home');
+    Route::get('/balance', 'BalanceController@index')->name('admin.balance');
 });
 
 
